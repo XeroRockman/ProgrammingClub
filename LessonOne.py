50ea87c4bd47e59a48860b1f2adceae22a0ce317
XeroRockman <andrewbrumbeloe@yahoo.com>
2023-01-26 10:14:26 -0600

    Added LessonOne.py
    In this lesson we learned how to create comments and print hello world to the console!
    We also learned how to create string and integer variables
    And how to use basic arithmetic operators in python.
    We also learned how to convert integers to a string variable and store the results to the screen.

---
+++ LessonOne.py
@@ -0,0 +1,64 @@
+#This is a comment 
+#here is another comment 
+
+#Creating Integers Variables
+#
+x = 1 
+#create an integer variable
+#name it x and assign it a value of 1
+
+y = 2 
+#create an integer variable
+#name it y and assign it a value of 2
+
+z = 3 
+#create an integer variable
+#name it z and assign it a value of 3
+
+
+#addition
+result = str(x + y + z)
+#add integer variables x, y, and z
+print("x + y + z = " + result)
+#print the result to the console
+
+
+#subtraction
+result = str(x - y - z)
+#subtract integer variables x, y, and z 
+print("x - y - z = " + result)
+#print the result to the console
+
+#multiplication
+result = str(x * y * z)
+#using the * operator
+#multiply integer variables x, y, and z 
+#store result as a string variable and name it "result"
+
+print("x * y * z = " + result)
+#print the result to the console
+
+#division
+result = str(z / y)
+#using the / operator
+#divide z by y by using the / operator
+print("z / y = " + result)
+#print the result to the console
+
+#modulus
+result = str(z % y)
+#using the (modulus)% operator
+#compute the remainder for z by y
+print("z % y = " + result)
+#print the result to the console
+
+
+#After your code compiles and runs successfully
+#If no other mistakes are found in your code
+#Your console should display the following results...
+#
+#x + y + z = 6
+#x - y - z = -4
+#x * y * z = 6
+#z / y = 1.5
+#z % y = 1
