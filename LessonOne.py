2db4a69280247c19f6211cbc3bc173b95fbaf887
XeroRockman <andrewbrumbeloe@yahoo.com>
2023-01-31 11:59:09 -0600

    changes

--- LessonOne.py
+++ LessonOne.py
@@ -1,5 +1,17 @@
+#
+#Programming Club
+#Lesson 1
+#Print: "Hello World", String Concatenation
+#Variables: Integers, Strings
+#Operators: (Arithmetic): add, subtract, multiply, divide
+#Comments: how to comment code
+#Concatenation: combining one string with another using the + operator
+#Functions: print() Print to the Console, str() Converting Integrs to a String 
+#
+
+
 #This is a comment 
-#here is another comment 
+#This is another comment 
 
 #Creating Integers Variables
 #
@@ -16,20 +28,30 @@ z = 3
 #name it z and assign it a value of 3
 
 
-#addition
+print("Hello World!")
+
+#concatenate strings using the + operator
+print("string" + " " + "another string")
+helloVar = "hello"
+worldVar = "world"
+#concatenate two predefined string variables
+concatVar = helloVar + worldVar
+print(concatVar)
+
+#addition operand
 result = str(x + y + z)
 #add integer variables x, y, and z
 print("x + y + z = " + result)
 #print the result to the console
 
 
-#subtraction
+#subtraction operand
 result = str(x - y - z)
 #subtract integer variables x, y, and z 
 print("x - y - z = " + result)
 #print the result to the console
 
-#multiplication
+#multiplication operand
 result = str(x * y * z)
 #using the * operator
 #multiply integer variables x, y, and z 
@@ -38,14 +60,14 @@ result = str(x * y * z)
 print("x * y * z = " + result)
 #print the result to the console
 
-#division
+#division operand
 result = str(z / y)
 #using the / operator
 #divide z by y by using the / operator
 print("z / y = " + result)
 #print the result to the console
 
-#modulus
+#modulus operand
 result = str(z % y)
 #using the (modulus)% operator
 #compute the remainder for z by y
